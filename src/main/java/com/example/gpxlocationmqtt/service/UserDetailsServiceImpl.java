863faf798ecc438c755d112d33d760d144743812
pablo <ekarlsson66@gmail.com>
2024-07-31 19:01:59 +0200

    structure and small fixes.

--- src/main/java/com/example/gpxlocationmqtt/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/gpxlocationmqtt/service/UserDetailsServiceImpl.java
@@ -13,7 +13,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Replace with your logic to load user from database
+        // Create logic to load user from database.
         return new org.springframework.security.core.userdetails.User(username, "$2a$10$7QJZT6eB0D1UV/oT8vUIUe9OGcsfv8g6XaXKZj09qlBz4fO6jIz9i", new ArrayList<>());
     }
 }
